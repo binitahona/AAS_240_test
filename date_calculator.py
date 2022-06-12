84ee4fddda01b0564dbcf06422047c3b575c56de
Angela Berti <aberti08@gmail.com>
2022-06-12 17:32:01 -0600

    date calculator edits

--- date_calculator.py
+++ date_calculator.py
@@ -1,37 +1,36 @@
 LEAST_FAVORITE_YEAR = 2020
 
-def suPerScr_etFunCtIon(month, day, year):
+def day_of_year(month, day, year):
     """
-    returns the day of year given a date
+    returns the day of year given a date in (month, day, year) format
     """
 
-    magic = day
+    num_days = day
     if month == 2:
-        magic = magic + 31
+        num_days = num_days + 31
     elif month == 3:
-        magic = magic + 59
+        num_days = num_days + 59
     elif month == 4:
-        magic = magic + 90
+        num_days = num_days + 90
     elif month == 5:
-        magic = magic + 31 + 28 + 31 + 30
+        num_days = num_days + 31 + 28 + 31 + 30
     elif month == 6:
-        magic = magic + 31 + 28 + 31 + 30 + 31
+        num_days = num_days + 31 + 28 + 31 + 30 + 31
     elif month == 7:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30
+        num_days = num_days + 31 + 28 + 31 + 30 + 31 + 30
     elif month == 8:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31
+        num_days = num_days + 31 + 28 + 31 + 30 + 31 + 30 + 31
     elif month == 9:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31
+        num_days = num_days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31
     elif month == 10:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30
+        num_days = num_days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30
     elif month == 11:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31
+        num_days = num_days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31
     elif month == 12:
-        magic = magic + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31
+        num_days = num_days + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31
     if year == LEAST_FAVORITE_YEAR:
-        magic = -1
-    print(magic)
-    return magic
+        num_days = -1
+    return num_days
 
 if __name__ == "__main__":
-    print(suPerScr_etFunCtIon(4, 1, 2022))
+    print(day_of_year(4, 1, 2022))
